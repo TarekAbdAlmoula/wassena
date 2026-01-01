d66287c419c6a97b4d301fd2456016dab21fafca
Tarek <abdalmoulatarek@gmail.com>
2026-01-01 15:15:39 +0300

    creata authGate widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:wassena/features/auth/ui/login_screen.dart';
 import 'package:wassena/features/home/ui/home_screen.dart';
 import 'package:wassena/firebase_options.dart';
 
@@ -19,11 +21,31 @@ class MyApp extends StatelessWidget {
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       builder: (context, child) {
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
+        return MaterialApp(debugShowCheckedModeBanner: false, home: AuthGate());
+      },
+    );
+  }
+}
 
-          home: HomeScreen(),
-        );
+class AuthGate extends StatefulWidget {
+  const AuthGate({super.key});
+
+  @override
+  State<AuthGate> createState() => _AuthGateState();
+}
+
+class _AuthGateState extends State<AuthGate> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, asyncSnapshot) {
+        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (asyncSnapshot.hasError) {
+          return Center(child: Text('حدث خطأ ما'));
+        }
+        return asyncSnapshot.hasData ? const HomeScreen() : const LoginScreen();
       },
     );
   }
