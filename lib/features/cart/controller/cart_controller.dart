3967aefff0d450bd9cee83c3263cf9deb7d8adf3
Tarek <abdalmoulatarek@gmail.com>
2026-01-08 17:26:04 +0300

    create cart screen

--- lib/features/cart/controller/cart_controller.dart
+++ lib/features/cart/controller/cart_controller.dart
@@ -1,25 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 import 'package:wassena/features/cart/model/item_cart.dart';
 import 'package:wassena/features/cart/model/orders.dart';
 
 class CartController extends ChangeNotifier {
   final List<ItemCart> items = [];
+  String orderId = '';
   Orders? orders;
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
 
   void addToCart(ItemCart itemCart) async {
     items.add(itemCart);
-    print('Item added to cart. Total items: ${items[0].restaurantId}');
   }
 
-  void createOrdersTable() {
+  void createOrdersTable() async {
     final user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      print(user.uid); // هذا هو User ID
-    } // هذا هو User ID
-
     orders = Orders(
       userId: user!.uid,
       restaurantId: items[0].restaurantId,
@@ -27,23 +24,59 @@ class CartController extends ChangeNotifier {
       totalAmount: items.fold(0, (sum, item) => sum + item.price * item.qty),
       createdAt: FieldValue.serverTimestamp(),
     );
-    _firebaseFirestore.collection('orders').add({
-      'userId': orders!.userId,
-      'restaurantId': orders!.restaurantId,
-      'orderStatus': orders!.orderStatus,
-      'totalAmount': orders!.totalAmount,
-      'createdAt': orders!.createdAt,
-    });
+    DocumentReference orderRef = await _firebaseFirestore
+        .collection('orders')
+        .add({
+          'userId': orders!.userId,
+          'restaurantId': orders!.restaurantId,
+          'orderStatus': orders!.orderStatus,
+          'totalAmount': orders!.totalAmount,
+          'createdAt': orders!.createdAt,
+        });
+    orderId = orderRef.id;
+    createOrderItemsTable(orderId: orderId);
   }
 
-  void printItems() {
-    for (var item in items) {
-      print('''restaurantId:${item.restaurantId}
-      itemName:${item.itemName}
-      price:${item.price}
-      imageUrl:${item.imageUrl}
-      qty:${item.qty}
-      ''');
+  void createOrderItemsTable({required String orderId}) async {
+    try {
+      QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('menu')
+          .where('data.restaurant_id', isEqualTo: items[0].restaurantId)
+          .limit(1)
+          .get();
+
+      if (snapshot.docs.isEmpty) {
+        return;
+      }
+      String menuId = snapshot.docs.first.id;
+
+      List<Map<String, dynamic>> orderItemsData = items.map((item) {
+        return {
+          'itemName': item.itemName,
+          'price': item.price,
+          'qty': item.qty,
+          'menuId': menuId,
+          'restaurantId': item.restaurantId,
+          'orderId': orderId,
+        };
+      }).toList();
+      await _firebaseFirestore.collection('order_items').add({
+        "orderItems": orderItemsData,
+      });
+    } catch (e) {
+      print(e.toString());
     }
+  }
+
+  void makePayment({required String paymentMethod}) async {
+    final uuid = Uuid();
+    String id = uuid.v4();
+    await _firebaseFirestore.collection('payments').add({
+      'order_id ': orderId,
+      'payment_method': paymentMethod,
+      'transaction_id': paymentMethod == 'الدفع كاش' ? '' : id,
+      'totalAmount': orders!.totalAmount,
+      'createdAt': FieldValue.serverTimestamp(),
+    });
   }
 }
--- lib/features/cart/ui/cart_screen.dart
+++ lib/features/cart/ui/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:wassena/features/cart/controller/cart_controller.dart';
 import 'package:wassena/utils/my_colors.dart';
@@ -16,7 +17,10 @@ class CartScreen extends StatelessWidget {
       );
     }
     return Scaffold(
-      appBar: AppBar(backgroundColor: MyColors.kMainColor),
+      appBar: AppBar(
+        backgroundColor: MyColors.kMainColor,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
       body: Container(
         margin: EdgeInsets.all(8),
         padding: EdgeInsets.all(10),
@@ -24,70 +28,158 @@ class CartScreen extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
           color: const Color.fromARGB(255, 237, 201, 200),
         ),
-        child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: cart.items.length,
-          itemBuilder: (context, index) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Text(
-                          cart.items[index].itemName,
-                          textAlign: TextAlign.end,
-                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              cart.items[index].itemName,
+                              textAlign: TextAlign.end,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text(
+                              'السعر: ${cart.items[index].price}  الكمية: ${cart.items[index].qty}',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                            Text('المطعم'),
+                          ],
                         ),
-                        Text(
-                          'السعر: ${cart.items[index].price}  الكمية: ${cart.items[index].qty}',
-                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        SizedBox(width: 5),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          height: 75,
+                          width: 100,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(cart.items[index].imageUrl),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+
+                          // margin: EdgeInsets.all(8),
                         ),
-                        Text('المطعم'),
                       ],
                     ),
-                    SizedBox(width: 5),
-                    Container(
-                      padding: EdgeInsets.all(10),
-                      height: 75,
-                      width: 100,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: NetworkImage(cart.items[index].imageUrl),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
 
-                      // margin: EdgeInsets.all(8),
-                    ),
+                    Divider(color: Colors.white, height: 10),
                   ],
+                );
+              },
+            ),
+            GestureDetector(
+              onTap: () {
+                try {
+                  context.read<CartController>().createOrdersTable();
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return SizedBox(
+                        height: 250,
+                        width: double.infinity,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(height: 20),
+                              Text('اختر طريقة الدفع'),
+                              SizedBox(height: 10),
+                              PaymentCard(
+                                paymentMethod: 'الدفع كاش',
+                                onTap: () {
+                                  context.read<CartController>().makePayment(
+                                    paymentMethod: 'الدفع كاش',
+                                  );
+                                },
+                              ),
+                              PaymentCard(
+                                onTap: () {
+                                  context.read<CartController>().makePayment(
+                                    paymentMethod: 'الدفع عن طريق البطاقة',
+                                  );
+                                },
+                                paymentMethod: 'الدفع عن طريق البطاقة',
+                              ),
+                              PaymentCard(
+                                onTap: () {
+                                  context.read<CartController>().makePayment(
+                                    paymentMethod: 'الدفع عن طريق paypal',
+                                  );
+                                },
+                                paymentMethod: 'الدفع عن طريق paypal',
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                } on Exception catch (e) {
+                  // TODO
+                }
+              },
+              child: Container(
+                height: 25,
+
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    context.read<CartController>().createOrdersTable();
-                  },
-                  child: Container(
-                    height: 25,
+                margin: EdgeInsets.symmetric(vertical: 10),
+                width: 90.w,
+                child: Center(child: Text('إكمال الطلب ')),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-                    decoration: BoxDecoration(
-                      color: Colors.white,
+class PaymentCard extends StatelessWidget {
+  final String paymentMethod;
+  final void Function()? onTap;
+  const PaymentCard({
+    super.key,
+    required this.paymentMethod,
+    required this.onTap,
+  });
 
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    width: 75,
-                    child: Center(child: Text('إكمال الطلب ')),
-                  ),
-                ),
-
-                // Divider(color: Colors.white, height: 10),
-              ],
-            );
-          },
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          height: 50.h,
+          width: double.infinity,
+          padding: EdgeInsets.all(5),
+          margin: EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Text(paymentMethod),
         ),
       ),
     );
